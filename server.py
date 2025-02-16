696eccce1e4a20630714bc5266e9262d4611de50
spencer burke <info.spencer.burke@gmail.com>
2025-02-15 21:01:53 -0800

    Add the VERY permissive cors policy so the frontend can request

--- server.py
+++ server.py
@@ -54,13 +54,13 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
