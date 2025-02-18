ea25489b6982d52df78e410863ae58afa586dd97
Ari Torczon <113228606+AriT000@users.noreply.github.com>
2025-02-18 14:57:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -104,18 +104,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://127.0.0.1",
-    "http://127.0.0.1:8000",
-    "https://minflixhd.web.app"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
