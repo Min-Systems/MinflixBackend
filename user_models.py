12b77aa7a9326fe8b26bbe55712e95f4a4a89af1
spencer burke <info.spencer.burke@gmail.com>
2025-03-02 22:14:08 -0800

    Add the working code

--- user_models.py
+++ user_models.py
@@ -1,6 +1,8 @@
 from sqlmodel import Field, SQLModel, Relationship
 from typing import List, Optional
+from pydantic import BaseModel
 from film_models import *
+
 
 class FilmUser(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -55,56 +57,3 @@ class WatchHistory(SQLModel, table=True):
     completion: Optional[int]
     datewatched: str
     profile: Profile = Relationship(back_populates="watch_history")
-
-
-class WatchHistoryRead(SQLModel):
-    id: int
-    profileid: int
-    film_id: int
-    timestamp: int
-    completion: int
-    datewatched: str
-
-
-class WatchLaterRead(SQLModel):
-    id: int
-    profileid: int
-    film_id: int
-    dateadded: str
-
-
-class FavoriteRead(SQLModel):
-    id: int
-    profileid: int
-    film_id: int
-    favorited_date: str
-
-
-class SearchHistoryRead(SQLModel):
-    id: int
-    profileid: int
-    search_query: str
-
-
-class ProfileRead(SQLModel):
-    id: int
-    filmuserid: int
-    displayname: str
-
-    search_history: List[SearchHistoryRead]
-    favorites: List[FavoriteRead]
-    watch_later: List[WatchLaterRead]
-    watch_history: List[WatchHistoryRead]
-
-
-class FilmUserRead(SQLModel):
-    id: int
-    email: str
-    password: str
-    date_registered: str
-    
-    profiles: List[ProfileRead]
-
-    class Config:
-        orm_mode = True
-
